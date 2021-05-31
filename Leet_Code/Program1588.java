d2c9972b239e474333db2d5b43a63dbe8f8a344d
AbhinavRaturi <abhinavraturi392@gmail.com>
2021-06-01 00:51:53 +0530

    new leet code added

--- Leet_Code/Program1588.java
+++ Leet_Code/Program1588.java
@@ -8,6 +8,22 @@ class Program1588 {
     }
 }
 
+/*
+ * index 0 1 2 3 4
+ * 
+ * val 1 4 2 5 3
+ * 
+ * start 5 4 3 2 1
+ * 
+ * end 1 2 3 4 5
+ * 
+ * tot(start*end) 5 8 9 8 5
+ * 
+ * odd 3 4 5 4 3
+ * 
+ * sum(odd*val) 3 16 10 20 9 = 58
+ * 
+ */
 class Solution1588 {
     public int sumOddLengthSubarrays(int[] arr) {
         int len = arr.length, sum = 0;
