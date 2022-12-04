be82f17cc45e321c1bf5c2da7205ff4a178eb757
AbhinavRaturi <abhinavraturi392@gmail.com>
2022-12-04 22:45:47 +0530

    Solved LeetCode 2256

---
+++ Leet_Code/Program2256.java
@@ -0,0 +1,32 @@
+package Leet_Code;
+
+public class Program2256 {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] nums = {4,6};
+        int result = s.minimumAverageDifference(nums);
+        System.out.println(result);   
+    }
+
+}
+class Solution {
+    public int minimumAverageDifference(int[] nums) {
+        int l = nums.length;  
+        int index = 0;
+        long min = Integer.MAX_VALUE, sum = 0, leftsum = 0, rightsum = 0;
+        for (int j = 0; j < l; j++) {
+            sum = sum + nums[j];
+        }
+        for (int i = 0; i < l; i++) {
+            leftsum += nums[i];
+            rightsum = sum - leftsum;
+            long diff = Math.abs(leftsum / (i + 1) - (l - i == 1 ? 0 : rightsum / (l - i - 1)));  // if number of rightsum values = 0
+            if (diff < min) {
+                min = diff;
+                index = i;
+            }
+        }
+        return index;
+
+    }
+}
