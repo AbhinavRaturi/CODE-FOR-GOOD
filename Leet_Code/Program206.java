5b38f6105042d354858a779ab5b25d702232ed43
US\araturi <araturi@deloitte.com>
2023-06-06 18:30:54 +0530

    Reverse Linked List

---
+++ Leet_Code/Program206.java
@@ -0,0 +1,14 @@
+package Leet_Code;
+
+public class Program206 {
+    public ListNode reverseList(ListNode head) {
+        ListNode newHead = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = newHead;
+            newHead = head;
+            head = next;
+        }
+        return newHead;
+    }
+}
